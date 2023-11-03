1dcc74e0cfe374fb3a003cb773cd16268a5d5ed9
vinsdesign <kelvinsaputra024@gmail.com>
2023-11-03 17:48:54 +0800

    memperbarui index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static('public'));
-// app.get("/", (req, res) => res.send("hello World"));
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.listen(8000);
